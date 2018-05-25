c0264f6da37f9fd3c8abb53cec2b198f6ce285bf
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-05-25 22:48:34 +0530

    final commit

---
+++ answer2.py
@@ -0,0 +1,6 @@
+length=float(input("Enter the length :"))
+breadth=float(input("Enter the breadth:"))
+if length==breadth:
+    print("The Dimension you have entered ",length," and ",breadth," are of square")
+else:
+     print("The Dimension you have entered ",length," and ",breadth," are of Rectangle")
